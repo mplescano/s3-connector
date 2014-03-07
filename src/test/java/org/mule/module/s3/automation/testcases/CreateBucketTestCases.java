5c7559b25ba32c897a560afc58012046c0f67eb8
Rodrigo López Dato <rodrigo.lopez@mulesoft.com>
2014-03-07 12:52:54 -0300

    Migrate CreateBucketTestCases to new framework

--- src/test/java/org/mule/module/s3/automation/testcases/CreateBucketTestCases.java
+++ src/test/java/org/mule/module/s3/automation/testcases/CreateBucketTestCases.java
@@ -1,97 +1,41 @@
-/**
- * Copyright (c) MuleSoft, Inc. All rights reserved. http://www.mulesoft.com
- *
- * The software in this package is published under the terms of the CPAL v1.0
- * license, a copy of which has been included with this distribution in the
- * LICENSE.md file.
- */
-
 package org.mule.module.s3.automation.testcases;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
-import java.util.HashMap;
-import java.util.UUID;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.mule.api.MuleEvent;
-import org.mule.api.processor.MessageProcessor;
 
 import com.amazonaws.services.s3.model.Bucket;
 
 public class CreateBucketTestCases extends S3TestParent {
 	
-	@Before
-	public void setUp(){
-		
-		testObjects = (HashMap<String,Object>) context.getBean("createBucketTestData");
+	private String bucketName;
 	
+	@Before
+	public void setUpBucketName() {
+		// Use the same bucket name for creation and deletion
+		bucketName = randomString();
+		initializeTestRunMessage("createBucketTestData");
+		upsertOnTestRunMessage("bucketName", bucketName);
 	}
 	
 	@After
-	public void tearDown() {
-		
-		try {
-				
-			MessageProcessor flow = lookupMessageProcessor("delete-bucket");
-			MuleEvent response = flow.process(getTestEvent(testObjects));
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-				e.printStackTrace();
-				fail();
-		}
-		
+	public void deleteCreatedBucket() throws Exception {
+		// TODO Rename when framework is updated
+		runFlowAndGetPayload("delete-bucket");
 	}
-
-    @Category({SmokeTests.class, SanityTests.class, RegressionTests.class})
+	
 	@Test
-	public void testCreateBucket() {
-    	
-    	testObjects.put("bucketName", UUID.randomUUID().toString());
-    	
-		MessageProcessor flow = lookupMessageProcessor("create-bucket");
-    	
-		try {
-
-			MuleEvent response = flow.process(getTestEvent(testObjects));
-			Bucket bucket = (Bucket) response.getMessage().getPayload();
-			
-			assertEquals(testObjects.get("bucketName").toString(), bucket.getName());
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			fail();
-		}
-     
+	public void createBucket() throws Exception {
+		Bucket createdBucket = runFlowAndGetPayload("create-bucket");
+		assertEquals(getTestRunMessageValue("bucketName"), createdBucket.getName());
 	}
-    
-    @Category({RegressionTests.class})
+	
 	@Test
-	public void testCreateBucketOptionalAttributes() {
-
-    	testObjects.put("bucketName", UUID.randomUUID().toString());
-    	
-		MessageProcessor flow = lookupMessageProcessor("create-bucket");
-    	
-		try {
-
-			MuleEvent response = flow.process(getTestEvent(testObjects));
-			Bucket bucket = (Bucket) response.getMessage().getPayload();
-			
-			assertEquals(testObjects.get("bucketName").toString(), bucket.getName());
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			fail();
-		}
-     
+	public void createBucketWithOptionalAttributes() throws Exception {
+		Bucket createdBucket = runFlowAndGetPayload("create-bucket-optional-attributes");
+		assertEquals(getTestRunMessageValue("bucketName"), createdBucket.getName());
 	}
-    
+	
 }
