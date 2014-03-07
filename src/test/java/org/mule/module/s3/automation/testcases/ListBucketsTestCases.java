cef5f241d804619ea7522425031a81c0a3f2d4f3
Rodrigo López Dato <rodrigo.lopez@mulesoft.com>
2014-03-07 14:58:40 -0300

    Migrate ListBucketsTestCases to new framework

---
+++ src/test/java/org/mule/module/s3/automation/testcases/ListBucketsTestCases.java
@@ -0,0 +1,52 @@
+package org.mule.module.s3.automation.testcases;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.amazonaws.services.s3.model.Bucket;
+
+public class ListBucketsTestCases extends S3TestParent {
+
+	private int bucketAmount;
+	private List<String> generatedBucketNames;
+
+	private List<String> createManyBuckets(int nBuckets) throws Exception {
+		List<String> bucketList = new ArrayList<String>();
+		for (int i = 0; i < nBuckets; i++) {
+			bucketList.add(createBucket(randomString()).getName());
+		}
+		return bucketList;
+	}
+
+	@Before
+	public void createRandomBuckets() {
+		initializeTestRunMessage("listBucketsTestData");
+		bucketAmount = Integer.parseInt(getTestRunMessageValue("bucketAmount")
+				.toString());
+	}
+
+	@After
+	public void deleteGeneratedBuckets() {
+		for (String bucketName : generatedBucketNames) {
+			upsertOnTestRunMessage("bucketName", bucketName);
+		}
+	}
+
+	@Test
+	// Create some buckets in S3, check if they are in the list
+	public void listBuckets() throws Exception {
+		generatedBucketNames = createManyBuckets(bucketAmount);
+		List<Bucket> bucketList = runFlowAndGetPayload("list-buckets");
+		List<String> bucketListNames = new ArrayList<String>();
+		for (Bucket b : bucketList) {
+			bucketListNames.add(b.getName());
+		}
+		assertTrue(bucketListNames.containsAll(generatedBucketNames));
+	}
+}
